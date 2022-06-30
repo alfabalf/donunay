ad1dc9df6c2691b8de6c41c3eeb3ac7dc1e7f874
alfabalf <georgesattler@gmail.com>
2022-07-01 08:01:09 +1000

    fix bucket create on startup

--- api/restapp/apps.py
+++ api/restapp/apps.py
@@ -9,15 +9,12 @@ class RestappConfig(AppConfig):
     name = 'restapp'
 
     def ready(self):
-        s3 = boto3.resource('s3', region_name=settings.AWS_REGION, endpoint_url=settings.ENDPOINT_URL)
+        client = boto3.client("s3", region_name=settings.AWS_REGION, endpoint_url=settings.ENDPOINT_URL)
+        try:
+            client.create_bucket(Bucket=settings.S3_BUCKET, CreateBucketConfiguration={'LocationConstraint': settings.AWS_REGION})
+            print("created bucket {}".format(settings.S3_BUCKET))
+        except Exception as e:
+            print(e)
 
-        if s3.Bucket(settings.S3_BUCKET) not in s3.buckets.all():
-            print("{bucket} bucket does not exist in {endpoint}"
-                  .format(bucket=settings.S3_BUCKET, endpoint=settings.ENDPOINT_URL))
-            s3.create_bucket(Bucket=settings.S3_BUCKET, CreateBucketConfiguration={'LocationConstraint': settings.AWS_REGION})
-            print("{bucket} created in {endpoint}"
-                  .format(bucket=settings.S3_BUCKET, endpoint=settings.ENDPOINT_URL))
-        else:
-            print("{bucket} already exists in {endpoint}"
-                  .format(bucket=settings.S3_BUCKET, endpoint=settings.ENDPOINT_URL))
+
 
